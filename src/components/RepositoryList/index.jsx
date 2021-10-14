f07585b7b5e7310a754f5c3f6a9e39637228ce27
gabrielhidro <gabrielhidro.dev@gmail.com>
2021-10-14 19:48:29 -0300

    fetch

--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -1,16 +1,25 @@
-import { createElement } from "react";
+import { createElement, useEffect, useState } from "react";
 import { RepositoryListPresentational } from "./presentational";
 
 function RepositoryList(){
   
+  const [ repositories, setRepositories ] = useState([])
+
   const repository = {
     name: 'unform',
     link: 'google'
   }
-  
-  return (
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/gabrielhidro/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+    }, [])
+    
+    return (
     createElement(RepositoryListPresentational, {
-      repository
+      repository,
+      repositories
     })
   )
 }
--- src/components/RepositoryList/presentational.jsx
+++ src/components/RepositoryList/presentational.jsx
@@ -3,12 +3,21 @@ import { Container } from "./styles";
 
 export function RepositoryListPresentational(props){
 
-  const {repository} = props
+  const {
+    repository,
+    repositories,
+  } = props
+
+  const repos = repositories.map(items => {
+    return (
+      <p>{items.name}</p>
+    )
+  })
 
   return (
     <Container>
       <h1>Lista de Repositórios</h1>
-
+      {repos}
       <ul>
         <RepositoryItem repository={repository} />
       </ul>
